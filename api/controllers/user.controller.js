ea5244c4fd91ee95c0439360280bdecfeebac364
unknown <Andres Felipe Herrera>
2020-04-28 10:11:07 -0500

    Adds the user controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,16 @@
 class UserController {
 
-  constructor() {}
+  constructor({UserService}) {
+    this._userService = UserService;
+  }
 
   sayHello(req, res) {
     return res.send({message: 'hola mundo'});
+  }
+
+  async getUsers() {
+    // services talks to the repository
+    return await this._userService.getUsers();
   }
   
 }
