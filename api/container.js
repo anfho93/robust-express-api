06c5f1edee23b0449bea57e164e121e3169c18f8
Andres Herrera <andres.h@smartenit.com>
2020-08-20 02:09:36 -0500

    Use the IoC to implement a container for Dependency Injection

--- api/container.js
+++ api/container.js
@@ -8,30 +8,50 @@ const config = require('../config/environments');
 
 // routes
 const Routes = require('./routes')
-const UserRoutes = require('./routes/user.routes');
+const StudentRoutes = require('./routes/student.routes');
+const TeacherRoutes = require('./routes/teacher.routes');
+const CourseRoutes = require('./routes/course.routes');
+const SubjectRoutes = require('./routes/subject.routes');
+const SectionRoutes = require('./routes/section.routes');
+const RegistrationRoutes = require('./routes/registration.routes');
 
 // repos
-const { UserRepository } = require('../dal/repositories');
+const { StudentRepository, TeacherRepository, CourseRepository,
+  SubjectRepository, SectionRepository, RegistrationRepository } = require('../dal/repositories');
 
 // controllers
-const { UserController } = require('./controllers')
+const { StudentController, TeacherController, CourseController,
+  SubjectController, SectionController, RegistrationController } = require('./controllers')
 
 // bussiness
-const { UserBussiness } = require('../domain')
+const { StudentBussiness, TeacherBussiness, CourseBussiness,
+  SubjectBussiness, SectionBussiness, RegistrationBussiness } = require('../domain')
 
 // services
-const { UserService } = require('../services');
+const { StudentService, TeacherService, CourseService,
+  SubjectService, SectionService, RegistrationService } = require('../services');
 
 // Container creation
 const container = createContainer();
-const db = require('../dal/entities')
+const db = require('../dal/models')
 
 container.register({
   app: asClass(StartUp).singleton(),
   router: asFunction(Routes).singleton(),
   server: asClass(Server).singleton(),
-  UserController: asClass(UserController).singleton(),
-  UserRoutes: asFunction(UserRoutes).singleton()
+  StudentController: asClass(StudentController).singleton(),
+  TeacherController: asClass(TeacherController).singleton(),
+  CourseController: asClass(CourseController).singleton(),
+  SubjectController: asClass(SubjectController).singleton(),
+  SectionController: asClass(SectionController).singleton(),
+  RegistrationController: asClass(RegistrationController).singleton(),
+
+  StudentRoutes: asFunction(StudentRoutes).singleton(),
+  TeacherRoutes: asFunction(TeacherRoutes).singleton(),
+  CourseRoutes: asFunction(CourseRoutes).singleton(),
+  SubjectRoutes: asFunction(SubjectRoutes).singleton(),
+  SectionRoutes: asFunction(SectionRoutes).singleton(),
+  RegistrationRoutes: asFunction(RegistrationRoutes).singleton(),
 })
   .register({
     config: asValue(config),
@@ -40,13 +60,28 @@ container.register({
     db: asValue(db)
   })
   .register({
-    UserService: asClass(UserService).singleton()
+    StudentService: asClass(StudentService).singleton(),
+    TeacherService: asClass(TeacherService).singleton(),
+    CourseService: asClass(CourseService).singleton(),
+    SubjectService: asClass(SubjectService).singleton(),
+    SectionService: asClass(SectionService).singleton(),
+    RegistrationService: asClass(RegistrationService).singleton()
   })
   .register({
-    UserRepository: asClass(UserRepository).singleton()
+    StudentRepository: asClass(StudentRepository).singleton(),
+    TeacherRepository: asClass(TeacherRepository).singleton(),
+    CourseRepository: asClass(CourseRepository).singleton(),
+    SubjectRepository: asClass(SubjectRepository).singleton(),
+    SectionRepository: asClass(SectionRepository).singleton(),
+    RegistrationRepository: asClass(RegistrationRepository).singleton()
   })
   .register({
-    UserBussiness: asClass(UserBussiness).singleton()
+    StudentBussiness: asClass(StudentBussiness).singleton(),
+    TeacherBussiness: asClass(TeacherBussiness).singleton(),
+    CourseBussiness: asClass(CourseBussiness).singleton(),
+    SubjectBussiness: asClass(SubjectBussiness).singleton(),
+    SectionBussiness: asClass(SectionBussiness).singleton(),
+    RegistrationBussiness: asClass(RegistrationBussiness).singleton()
   })
 
 module.exports = container;
